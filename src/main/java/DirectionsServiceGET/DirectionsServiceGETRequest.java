61dc6f5a0b831ca18284f4335a81050f2c5f30aa
Blake Sooter <jbsooter@uark.edu>
2022-04-22 19:48:35 -0500

    Set up object oriented request structure for directions get and post
    Convenience wrappers around the API endpoints for most common (specifically in my use case) components

--- src/main/java/DirectionsServiceGET/DirectionsServiceGETRequest.java
+++ src/main/java/DirectionsServiceGET/DirectionsServiceGETRequest.java
@@ -10,12 +10,45 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class DirectionsServiceGETRequest {
-    public static DirectionsServiceGETResult getDirections(double latA, double lonA,double latB, double lonB, String profile, HttpClient clientConn) throws URISyntaxException, IOException, InterruptedException {
+    /**
+     * Point A Latitude
+     */
+    Double latA;
+
+    /**
+     * Point A Longitude
+     */
+    Double lonA;
+
+    /**
+     * Point B Latitude
+     */
+    Double latB;
+
+    /**
+     * Point B Longitude
+     */
+    Double lonB;
+
+    /**
+     * Shared connection to use from Java net package.
+     */
+    HttpClient clientConn;
+
+    public DirectionsServiceGETRequest(Double latA, Double lonA, Double latB, Double lonB, HttpClient clientConn) {
+        this.latA = latA;
+        this.lonA = lonA;
+        this.latB = latB;
+        this.lonB = lonB;
+        this.clientConn = clientConn;
+    }
+
+    public DirectionsServiceGETResult getDirections() throws URISyntaxException, IOException, InterruptedException {
         //request route
         HttpRequest orsReq = HttpRequest.newBuilder(
                 new URI(String.format(
                                 "http://localhost:8080/ors/v2/directions/%s?api_key=your-api-key&start=%s,%s&end=%s,%s",
-                        profile,lonA, latA, lonB, latB))).build();
+                        "driving-car",lonA, latA, lonB, latB))).build();
         HttpResponse<String> orsResp = clientConn.send(orsReq, HttpResponse.BodyHandlers.ofString());
 
         //if request successful, map to object with Jackson
--- src/main/java/DirectionsServicePOST/DirectionsServicePOSTRequest.java
+++ src/main/java/DirectionsServicePOST/DirectionsServicePOSTRequest.java
@@ -11,12 +11,126 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class DirectionsServicePOSTRequest {
-    public static DirectionsServicePOSTResult postDirections(double latA, double lonA,double latB, double lonB, String profile, HttpClient clientConn) throws URISyntaxException, IOException, InterruptedException {
+    /**
+     * Point A Latitude
+     */
+    private Double latA;
+
+    /**
+     * Point A Longitude
+     */
+    private Double lonA;
+
+    /**
+     * Point B Latitude
+     */
+    private Double latB;
+
+    /**
+     * Point B Longitude
+     */
+    private Double lonB;
+
+    /**
+     * Maximum fraction of the route that alternatives may share with the optimal route. The default value of 0.6 means alternatives can share up to 60% of path segments with the optimal route.
+     */
+    private Double alternative_routes_share_factor;
+
+    /**
+     * Target number of alternative routes to compute. Service returns up to this number of routes that fulfill the share-factor and weight-factor constraints.
+     */
+    private Integer alternative_routes_target_count;
+
+    /**
+     * Maximum factor by which route weight may diverge from the optimal route. The default value of 1.4 means alternatives can be up to 1.4 times longer (costly) than the optimal route.
+     */
+    private Double alternative_routes_weight_factor;
+
+    /**
+     * TRUE: Include average speed along a segment.
+     * FALSE: Do not include.
+     */
+    private Boolean avgspeed;
+
+    /**
+     * TRUE: Include elevation data in the response body.
+     * FALSE: DO not Include
+     */
+    private Boolean elevation;
+
+    /**
+     * Units of distance to use in response body.
+     * Options "m", "km", "mi"
+     */
+    private String units;
+
+    /**
+     * Shared connection to use from Java net package.
+     */
+    private HttpClient clientConn;
+
+    public DirectionsServicePOSTRequest(Double latA, Double lonA, Double latB, Double lonB, HttpClient clientConn) {
+        this.latA = latA;
+        this.lonA = lonA;
+        this.latB = latB;
+        this.lonB = lonB;
+        this.clientConn = clientConn;
+        this.avgspeed = false;
+        this.elevation = false;
+        this.units = null;
+        this.alternative_routes_target_count = null;
+        this.alternative_routes_weight_factor = null;
+        this.alternative_routes_share_factor = null;
+    }
+
+    public DirectionsServicePOSTRequest(Double latA, Double lonA, Double latB, Double lonB, Double alternative_routes_share_factor, Integer alternative_routes_target_count, Double alternative_routes_weight_factor, Boolean avgspeed, Boolean elevation, String units, HttpClient clientConn) {
+        this.latA = latA;
+        this.lonA = lonA;
+        this.latB = latB;
+        this.lonB = lonB;
+        this.alternative_routes_share_factor = alternative_routes_share_factor;
+        this.alternative_routes_target_count = alternative_routes_target_count;
+        this.alternative_routes_weight_factor = alternative_routes_weight_factor;
+        this.avgspeed = avgspeed;
+        this.elevation = elevation;
+        this.units = units;
+        this.clientConn = clientConn;
+    }
+
+    public  DirectionsServicePOSTResult postDirections() throws URISyntaxException, IOException, InterruptedException {
+        String requestBody = String.format("{\"coordinates\":[[%s,%s],[%s,%s]]", lonA, latA, lonB, latB);
+        if(alternative_routes_share_factor != null && alternative_routes_target_count != null && alternative_routes_weight_factor != null)
+        {
+            requestBody += String.format(",\"alternative_routes\":{\"share_factor\":%s,\"target_count\":%s,\"weight_factor\":%s}}"
+                    , alternative_routes_share_factor
+                    , alternative_routes_target_count
+                    , alternative_routes_weight_factor);
+        }
+
+        if(avgspeed)
+        {
+            requestBody += "\"attributes\":[\"avgspeed\"]";
+        }
+
+        if(elevation)
+        {
+            requestBody += ",\"elevation\":\"true\"";
+        }
+
+        if(units != null)
+        {
+            requestBody+= String.format(",\"units\":\"%s\"", units);
+        }
+
+        requestBody += "}";
+
+        System.out.println(requestBody);
+
         HttpRequest orsReq = HttpRequest.newBuilder(
                         new URI("http://localhost:8080/ors/v2/directions/driving-car"))
                 .header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
                 .header("Content-Type", "application/json; charset=utf-8")
-                .POST(HttpRequest.BodyPublishers.ofString("{\"coordinates\":[[-94.0,36.2],[-94.09,36.2]],\"geometry\":\"false\",\"alternative_routes\":{\"target_count\":2,\"weight_factor\":1.6},\"elevation\":\"true\",\"extra_info\":[\"steepness\"]}")).build();
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody)).build();
 
         HttpResponse<String> orsResp = clientConn.send(orsReq,HttpResponse.BodyHandlers.ofString());
         //if request successful, map to object with Jackson
@@ -29,8 +143,11 @@ public class DirectionsServicePOSTRequest {
         //o.w., return null object
         else
         {
+
             return null;
         }
     }
+
+
 
 }
